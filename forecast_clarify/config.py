22e2fc11f9f316235b04ad117335c1549cbb01b9
olewu <owul@norceresearch.no>
2022-10-05 18:57:14 +0200

    update config

--- forecast_clarify/config.py
+++ forecast_clarify/config.py
@@ -1,11 +1,11 @@
 import os
 
-base = '/projects/NS9001K/owul/'
+base = '/projects/NS9001K/owul/projects/forecast_clarify/'
 
 dirs = dict(
-    proj_base   = os.path.join(base,'projects/forecast_clarify/'),
-    figures     = os.path.join(base,'figures/S2S_fishfarm/'),
+    proj_base   = base,
+    figures     = os.path.join(base,'data/processedfigures/'),
+    param_files = os.path.join(base,'data/processed/')
 )
-dirs['param_files'] = os.path.join(dirs['proj_base'],'data/processed/')
 
 bw_sites_file = os.path.join(dirs['proj_base'],'data/external/barentswatch_sites.json')
